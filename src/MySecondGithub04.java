90c6fc035e9ded07037b6cc619c5cb8a23ea39c3
chamaros <hkarakas04@gmail.com>
2022-08-13 20:27:14 +0300

    except fourth and six

---
+++ src/MySecondGithub04.java
@@ -0,0 +1,7 @@
+public class MySecondGithub04 {
+    public static void main(String[] args) {
+        System.out.println("this is the first type from intellij");
+        System.out.println("Iam adding second change");
+        System.out.println("Iam adding bir tane daha change");
+    }
+}
---
+++ src/MySecondGithub06.java
@@ -0,0 +1,6 @@
+public class MySecondGithub06 {
+    public static void main(String[] args) {
+
+        System.out.println("this class will not go to github");
+    }
+}
