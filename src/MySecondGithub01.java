64650faf185c290390904a08463b953a0b7e7148
chamaros <hkarakas04@gmail.com>
2022-08-13 15:35:01 +0300

    2.denemem githuba aktarim denemem

--- src/MySecondGithub01.java
+++ src/MySecondGithub01.java
@@ -1,2 +1,7 @@
 public class MySecondGithub01 {
+    public static void main(String[] args) {
+        System.out.println("this is the first type from intellij");
+        System.out.println("Iam adding second change");
+        System.out.println("Iam adding bir tane daha change");
+    }
 }
