d1ecf5d5550be0181d2e4720e17327a66aa6270c
Chamaros <110726366+Chamaros@users.noreply.github.com>
2022-08-13 21:38:05 +0300

    Update MySecondGithub06.java

--- src/MySecondGithub06.java
+++ src/MySecondGithub06.java
@@ -2,5 +2,7 @@ public class MySecondGithub06 {
     public static void main(String[] args) {
 
         System.out.println("this class will not go to github");
+        System.out.println("we wrote this in GITHUB ");
+        
     }
 }
