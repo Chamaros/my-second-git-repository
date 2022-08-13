5a3c29bff6077088d03cf0f28233d458bfbf05e2
chamaros <hkarakas04@gmail.com>
2022-08-13 19:58:49 +0300

    third java

---
+++ src/MySecondGithub03.java
@@ -0,0 +1,7 @@
+public class MySecondGithub03 {
+    public static void main(String[] args) {
+        System.out.println("this is the first type from intellij");
+        System.out.println("Iam adding second change");
+        System.out.println("Iam adding bir tane daha change");
+    }
+}
