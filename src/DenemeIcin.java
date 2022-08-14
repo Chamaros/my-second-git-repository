e90647e8c890c38326b7538f3cf3c3be4ed98ca0
Chamaros <110726366+Chamaros@users.noreply.github.com>
2022-08-14 22:12:42 +0300

    Update DenemeIcin.java

--- src/DenemeIcin.java
+++ src/DenemeIcin.java
@@ -11,7 +11,7 @@ public class DenemeIcin {
         System.out.println("Iam adding bir tane daha change");
         System.out.println("we wrote this in GITHUB ");
          System.out.println("we wrote this in GITHUB 2 ");
-        // MAIN DE YAZDIM.
+    
         
 
     }
