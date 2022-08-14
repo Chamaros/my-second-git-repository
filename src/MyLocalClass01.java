3e0f03966ecbdbabe5a999f75f490b4c42f87d5d
chamaros <hkarakas04@gmail.com>
2022-08-14 20:56:26 +0300

    my local eklendi

---
+++ src/MyLocalClass01.java
@@ -0,0 +1,10 @@
+public class MyLocalClass01 {
+    public static void main(String[] args) {
+        System.out.println("this is the first type from intellij");
+        System.out.println("Iam adding second change");
+        System.out.println("Iam adding bir tane daha change");
+        System.out.println("we wrote this in GITHUB ");
+         System.out.println("we wrote this in GITHUB 2 ");
+        
+    }
+}
